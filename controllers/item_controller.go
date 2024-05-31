e953154e9846ccc7b76ef11462f7f1f79724d1c1
Kagami <shimura.kyohei7617@gmail.com>
2024-05-31 23:56:23 +0900

    auto: auto command push

--- controllers/item_controller.go
+++ controllers/item_controller.go
@@ -1,7 +1,19 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"gin-fleamarket/services"
+
+	"github.com/gin-gonic/gin"
+)
 
 type IItemController interface {
 	FindAll(ctx *gin.Context)
+}
+
+type IItemController struct {
+	service services.IItemService
+}
+
+func NewItemController(service services.IItemService) IItemController {
+	return &IItemController{service: service}
 }
