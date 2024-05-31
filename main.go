ff3ca8dcad2e63aa8e4b36e549040a17f8805884
Kagami <shimura.kyohei7617@gmail.com>
2024-05-31 22:21:20 +0900

    auto: auto command push

--- main.go
+++ main.go
@@ -1,10 +1,20 @@
 package main
 
 import (
+	"gin-fleamarket/models"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	items := []models.Item{
+		{ID: 1,
+			Name:        "商品1",
+			Price:       1000,
+			Description: "説明1",
+			SoldOut:     false,
+		},
+	}
 	// Ginのルーターを初期化
 	r := gin.Default()
 	// path, 無名関数
