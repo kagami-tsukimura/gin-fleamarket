3b52454b15dd067497d18f5716944e15489a30cd
Kagami <shimura.kyohei7617@gmail.com>
2024-05-31 23:58:08 +0900

    auto: auto command push

--- controllers/item_controller.go
+++ controllers/item_controller.go
@@ -23,6 +23,7 @@ func (c *ItemController) FindAll(ctx *gin.Context) {
 	items, err := c.service.FindAll()
 	if err != nil {
 		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Unexpected error"})
+		// early return
 		return
 	}
 	ctx.JSON(200, items)
