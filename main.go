9df2fa4e635d745323433f217795da2ca73ae8f8
Kagami <shimura.kyohei7617@gmail.com>
2024-06-01 06:46:22 +0900

    auto: auto command push

--- main.go
+++ main.go
@@ -22,14 +22,14 @@ func main() {
 
 	// Ginのルーターを初期化
 	r := gin.Default()
-	// // path, 無名関数
-	// r.GET("/ping", func(c *gin.Context) {
-	// 	// status_code, body
-	// 	// gin.H: map
-	// 	c.JSON(200, gin.H{
-	// 		"message": "pong",
-	// 	})
-	// })
+	// path, 無名関数
+	r.GET("/ping", func(c *gin.Context) {
+		// status_code, body
+		// gin.H: map
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
 	r.GET("/items", itemController.FindAll)
 	r.Run("localhost:8080") // default: 0.0.0.0:8080 でサーバーを立てます。
 }
