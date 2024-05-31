27032dfba4e7ace6e6c39b9d4888b57f3d736c2e
Kagami <shimura.kyohei7617@gmail.com>
2024-05-31 23:54:15 +0900

    auto: auto command push

--- services/item_service.go
+++ services/item_service.go
@@ -13,3 +13,7 @@ type ItemService struct {
 	// Interface型で実装の切り替えを容易にする
 	repository repositories.IItemRepository
 }
+
+func NewItemService(repository repositories.IItemRepository) IItemService {
+	return &ItemService{repository: repository}
+}
