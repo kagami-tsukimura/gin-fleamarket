e2fa206ec9f838a22277b03946b8eca10c7aae75
Kagami <shimura.kyohei7617@gmail.com>
2024-06-01 06:37:30 +0900

    auto: auto command push

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"gin-fleamarket/controllers"
+	"gin-fleamarket/repositories"
+	"gin-fleamarket/services"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -10,6 +14,10 @@ func main() {
 		{ID: 2, Name: "商品2", Price: 2000, Description: "説明2", SoldOut: true},
 		{ID: 3, Name: "商品3", Price: 3000, Description: "説明3", SoldOut: false},
 	}
+
+	itemRepository := repositories.NewItemMemoryRepository(items)
+	ItemService := services.NewItemService(itemRepository)
+	itemController := controllers.NewItemController(ItemService)
 
 	// Ginのルーターを初期化
 	r := gin.Default()
