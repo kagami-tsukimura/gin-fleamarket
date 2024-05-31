bba928ca0ad6b8c8360e126d91b2975a499aeb27
Kagami <shimura.kyohei7617@gmail.com>
2024-05-31 23:53:33 +0900

    auto: auto command push

--- services/item_service.go
+++ services/item_service.go
@@ -1,7 +1,15 @@
 package services
 
-import "gin-fleamarket/models"
+import (
+	"gin-fleamarket/models"
+	"gin-fleamarket/repositories"
+)
 
 type IItemService interface {
 	FindAll() (*[]models.Item, error)
+}
+
+type ItemService struct {
+	// Interface型で実装の切り替えを容易にする
+	repository repositories.IItemRepository
 }
