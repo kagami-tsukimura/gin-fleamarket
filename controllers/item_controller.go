219dbf5e52a3037bd0b2646991dc8816726fa283
Kagami <shimura.kyohei7617@gmail.com>
2024-05-31 23:57:22 +0900

    auto: auto command push

--- controllers/item_controller.go
+++ controllers/item_controller.go
@@ -17,3 +17,12 @@ type IItemController struct {
 func NewItemController(service services.IItemService) IItemController {
 	return &IItemController{service: service}
 }
+
+func (c *ItemController) FindAll(ctx *gin.Context) {
+	items, err := c.service.FindAll()
+	if err != nil {
+		ctx.JSON(500, gin.H{"error": err.Error()})
+		return
+	}
+	ctx.JSON(200, items)
+}
