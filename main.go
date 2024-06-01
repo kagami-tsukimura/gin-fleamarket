78044932084a06e7cc2c3957557a0705c679b400
Kagami <shimura.kyohei7617@gmail.com>
2024-06-01 17:59:18 +0900

    auto: Grouping

--- main.go
+++ main.go
@@ -27,10 +27,11 @@ func main() {
 
 	// Ginのルーターを初期化
 	r := gin.Default()
-	r.GET("/items", itemController.FindAll)
-	r.GET("/items/:id", itemController.FindById)
-	r.POST("/items", itemController.Create)
-	r.PUT("/items/:id", itemController.Update)
-	r.DELETE("/items/:id", itemController.Delete)
+	itemRouter := r.Group("/items")
+	itemRouter.GET("", itemController.FindAll)
+	itemRouter.GET("/:id", itemController.FindById)
+	itemRouter.POST("", itemController.Create)
+	itemRouter.PUT("/:id", itemController.Update)
+	itemRouter.DELETE("/:id", itemController.Delete)
 	r.Run("localhost:8080") // default: 0.0.0.0:8080 でサーバーを立てます。
 }
