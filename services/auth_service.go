6ec4bbb8e605630b9b69785809c5ab8da687bd91
Kagami <shimura.kyohei7617@gmail.com>
2024-06-01 22:13:51 +0900

    auto: decode jwt

--- services/auth_service.go
+++ services/auth_service.go
@@ -77,5 +77,6 @@ func (s *AuthService) GetUserFromToken(tokenString string) (*models.User, error)
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
 			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
 		}
+		return []byte(os.Getenv("SECRET_KEY")), nil
 	})
 }
