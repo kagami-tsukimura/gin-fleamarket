5c8836e797ea1a5248a620825013790da9533858
Kagami <shimura.kyohei7617@gmail.com>
2024-06-01 11:13:51 +0900

    auto: test comment revival

--- dto/item_dto.go
+++ dto/item_dto.go
@@ -8,6 +8,7 @@ type CreateItemInput struct {
 }
 
 type UpdateItemInput struct {
+	// validator: ``でvalidation check
 	Name        string `json:"name" binding:"required,min=2"`
 	Price       uint   `json:"price" binding:"required,min=1,max=999999"`
 	Description string `json:"description"`
