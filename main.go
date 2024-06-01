88d6b0d417587b776dae1c94ea9f51e2ea00784d
Kagami <shimura.kyohei7617@gmail.com>
2024-06-01 21:33:20 +0900

    auto: auth main

--- main.go
+++ main.go
@@ -25,13 +25,21 @@ func main() {
 	ItemService := services.NewItemService(itemRepository)
 	itemController := controllers.NewItemController(ItemService)
 
+	authRepository := repositories.NewAuthRepository(db)
+	authService := services.NewAuthService(authRepository)
+	authController := controllers.NewAuthController(authService)
+
 	// Ginのルーターを初期化
 	r := gin.Default()
 	itemRouter := r.Group("/items")
+	authRouter := r.Group("/auth")
+
 	itemRouter.GET("", itemController.FindAll)
 	itemRouter.GET("/:id", itemController.FindById)
 	itemRouter.POST("", itemController.Create)
 	itemRouter.PUT("/:id", itemController.Update)
 	itemRouter.DELETE("/:id", itemController.Delete)
+
+	authRouter.POST("/signup", authController.Signup)
 	r.Run("localhost:8080") // default: 0.0.0.0:8080 でサーバーを立てます。
 }
