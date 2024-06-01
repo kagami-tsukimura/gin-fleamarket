2d08abd4d625d3a7d264fdadfa2f9ffd72aed220
Kagami <shimura.kyohei7617@gmail.com>
2024-06-01 21:24:17 +0900

    auto: auth ser

--- services/auth_service.go
+++ services/auth_service.go
@@ -1,6 +1,11 @@
 package services
 
-import "gin-fleamarket/repositories"
+import (
+	"gin-fleamarket/models"
+	"gin-fleamarket/repositories"
+
+	"golang.org/x/crypto/bcrypt"
+)
 
 type IAuthService interface {
 	Signup(email string, password string) error
@@ -10,6 +15,20 @@ type AuthService struct {
 	repository repositories.IAuthRepository
 }
 
-func NewAuthService(rpository repositories.IAuthRepository) IAuthService {
-	return &AuthService{repository: rpository}
+func NewAuthService(repository repositories.IAuthRepository) IAuthService {
+	return &AuthService{repository: repository}
+}
+
+func (s *AuthService) Signup(email string, password string) error {
+	// パスワードのハッシュ化
+	// bcrypt.DefaultCost: セキュリティとパフォーマンスのバランスを取る
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+	user := models.User{
+		Email:    email,
+		Password: string(hashedPassword),
+	}
+	return s.repository.CreateUser(user)
 }
