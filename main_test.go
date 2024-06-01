1f4a8a8ea076b6e087f16cb6778e4e05f285677e
Kagami <shimura.kyohei7617@gmail.com>
2024-06-02 08:51:32 +0900

    auto: test items

--- main_test.go
+++ main_test.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"gin-fleamarket/models"
 	"log"
 	"os"
 	"testing"
 
 	"github.com/joho/godotenv"
+	"gorm.io/gorm"
 )
 
 func TestMain(m *testing.M) {
@@ -16,4 +18,12 @@ func TestMain(m *testing.M) {
 	code := m.Run()
 
 	os.Exit(code)
+}
+
+func setupTestData(db *gorm.DB) {
+	items := []models.Item{
+		{Name: "テストアイテム1", Price: 1000, Description: "", SoldOut: false, UserID: 1},
+		{Name: "テストアイテム2", Price: 2000, Description: "テスト2", SoldOut: true, UserID: 1},
+		{Name: "テストアイテム3", Price: 3000, Description: "テスト3", SoldOut: false, UserID: 2},
+	}
 }
