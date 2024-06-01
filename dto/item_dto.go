2d72f49cfbff26b087a65bb4f4a5311aceaab117
Kagami <shimura.kyohei7617@gmail.com>
2024-06-01 11:12:28 +0900

    auto: rm

--- dto/item_dto.go
+++ dto/item_dto.go
@@ -8,7 +8,6 @@ type CreateItemInput struct {
 }
 
 type UpdateItemInput struct {
-	// validator: ``でvalidation check
 	Name        string `json:"name" binding:"required,min=2"`
 	Price       uint   `json:"price" binding:"required,min=1,max=999999"`
 	Description string `json:"description"`
