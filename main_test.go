f55813c0cf8c91204d0bc45b5d7c4ea6ca92590e
Kagami <shimura.kyohei7617@gmail.com>
2024-06-02 10:44:27 +0900

    auto: create test

--- main_test.go
+++ main_test.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
+	"gin-fleamarket/dto"
 	"gin-fleamarket/infra"
 	"gin-fleamarket/models"
+	"gin-fleamarket/services"
 	"log"
 	"net/http"
 	"net/http/httptest"
@@ -71,19 +74,51 @@ func TestFindAll(t *testing.T) {
 	assert.Equal(t, 3, len(res["data"]))
 }
 
-func TestFindById(t *testing.T) {
+func TestCreate(t *testing.T) {
 	// テストのセットアップ
 	router := setup()
 
+	// 認証
+	token, err := services.CreateToken(1, "test1@example.com")
+	// エラー確認
+	assert.Equal(t, nil, err)
+
+	createItemInput := dto.CreateItemInput{
+		Name:        "テストアイテム4",
+		Price:       4000,
+		Description: "Create Test",
+	}
+	// json encode
+	reqBody, _ := json.Marshal(createItemInput)
+
 	w := httptest.NewRecorder()
 	// Request, Path, Body
-	req, _ := http.NewRequest("GET", "/items/1", nil)
+	req, _ := http.NewRequest("POST", "/items", bytes.NewBuffer(reqBody))
+	req.Header.Set("Authorization", "Bearer "+*token)
+
 	// テストの実行
 	router.ServeHTTP(w, req)
 	// レスポンスの検証
-	var res map[string][]models.Item
+	var res map[string]models.Item
 	json.Unmarshal(w.Body.Bytes(), &res)
 	// assertion
-	assert.Equal(t, http.StatusUnauthorized, w.Code)
-	assert.Equal(t, 0, len(res["data"]))
+	assert.Equal(t, http.StatusCreated, w.Code)
+	assert.Equal(t, uint(4), res["data"].ID)
 }
+
+// func TestFindById(t *testing.T) {
+// 	// テストのセットアップ
+// 	router := setup()
+
+// 	w := httptest.NewRecorder()
+// 	// Request, Path, Body
+// 	req, _ := http.NewRequest("GET", "/items/1", nil)
+// 	// テストの実行
+// 	router.ServeHTTP(w, req)
+// 	// レスポンスの検証
+// 	var res map[string][]models.Item
+// 	json.Unmarshal(w.Body.Bytes(), &res)
+// 	// assertion
+// 	assert.Equal(t, http.StatusUnauthorized, w.Code)
+// 	assert.Equal(t, 0, len(res["data"]))
+// }
